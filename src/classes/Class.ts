368c849327657057658462e7fab0adad352ae00a
Emir Guvenni <contact@emirguvenni.com>
2022-04-18 03:37:34 +0300

    Added example scripts

---
+++ src/classes/Class.ts
@@ -0,0 +1,5 @@
+export default class Class {
+  public static sayHelloWorld() {
+    console.log('Hello, World!');
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import Class from '@class/Class';
+
+Class.sayHelloWorld();
